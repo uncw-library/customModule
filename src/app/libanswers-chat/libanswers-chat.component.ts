db8d89a2d2532163cede45f063142970f25ab1ad
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2026-04-13 16:10:43 -0400

    libanswers chat small delay to allow its loading page

--- src/app/libanswers-chat/libanswers-chat.component.ts
+++ src/app/libanswers-chat/libanswers-chat.component.ts
@@ -24,16 +24,18 @@ export class LibAnswersChatComponent implements OnInit {
       protocol +
       'uncw.libanswers.com/load_chat.php?hash=c29df0495cb2f734e42785dbb34e4d07ed30d46dcae5f249a035fcac5f3a4581';
 
-    // after loaded, do a click on the button to open the window (to default it to open)
+    // a moment after loaded, do a click on the button to open the window (to default it to open)
     lc.addEventListener('load', () => {
-      const observer = new MutationObserver(() => {
-        const btn = document.getElementById('lcs_slide_out_button-27127') as HTMLElement | null;
-        if (btn) {
-          btn.click();
-          observer.disconnect();
-        }
-      });
-      observer.observe(document.body, { childList: true, subtree: true });
+      setTimeout(() => {
+        const observer = new MutationObserver(() => {
+          const btn = document.getElementById('lcs_slide_out_button-27127') as HTMLElement | null;
+          if (btn) {
+            btn.click();
+            observer.disconnect();
+          }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+      }, 5000);
     });
 
     const s = document.getElementsByTagName('script')[0];
