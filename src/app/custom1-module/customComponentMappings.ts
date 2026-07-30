382006c2da6ca3af4f782b216da43d18762b705b
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2026-07-30 14:50:17 -0400

    removed libchat module so new libchat could be applied

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,12 +1,10 @@
 import { UncwHeaderComponent } from "../uncw-header/uncw-header.component";
-// import { LibAnswersChatComponent } from "../libanswers-chat/libanswers-chat.component";
 import { HideLoginSnackbarComponent } from "../hide-login-snackbar/hide-login-snackbar.component";
 import { TwilioSMSComponent } from "../twilio-sms/twilio-sms.component";
 
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
     ['nde-header-top', UncwHeaderComponent],
-    // ['nde-header-after', LibAnswersChatComponent],
     ['nde-header-before', HideLoginSnackbarComponent],
     ['nde-location-after', TwilioSMSComponent],
 
